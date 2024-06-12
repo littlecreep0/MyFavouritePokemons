90c190aebb605d3d14f52a5889be0e74777da528
unknown <enikitin575@gmail.com>
2024-06-12 22:16:54 +0300

    into the repository you go

---
+++ app/src/main/java/lc/wise/myfavouritepokemons/MyApp.kt
@@ -0,0 +1,11 @@
+package lc.wise.myfavouritepokemons
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
